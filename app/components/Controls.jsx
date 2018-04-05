6ae6416b69336fca8c7b26c88edca700d0d36c37
Jorge <barbozajorge@gmail.com>
2018-04-05 15:21:50 -0400

    Finalize Test and add page title

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -10,9 +10,6 @@ var Controls = React.createClass({
       this.props.onStatusChange(newStatus);
     }
   },
-  componentWillReceiveProps: function(newProps){
-    console.log('component will receive props: ', newProps.countdownStatus);
-  },
   render: function(){
     var {countdownStatus} = this.props;
     var renderStartStopButton = ()=>{
